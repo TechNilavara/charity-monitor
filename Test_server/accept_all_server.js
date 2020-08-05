ce243d30ab300701599f050d8d2270086acd422b
007alans <007alans@gmail.com>
2020-08-05 17:28:39 +0530

    Added BodyParser

--- Test_server/accept_all_server.js
+++ Test_server/accept_all_server.js
@@ -5,17 +5,20 @@ var Http = require('http');
 var Express = require('express');
 var Path = require('path');
 var file = require('./filer')
-
+const bodyParser = require('body-parser');
 var App = Express();
 var Server = Http.createServer(App);
+
+App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({extended: true}) );
 
 App.use(function(req, res) {  // Enable cross origin resource sharing (for app frontend)
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     file.log(req)
-    console.log(req.method)
-    
+    console.log(req.body)
+    res.send(200)
 });
 
 Server.listen(port, function () {  // Starts server with our modfied port settings
